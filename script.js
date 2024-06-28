431782a515d3d43a81a181a1271abcb06d20fea3
Kumar Sonu Nigam <156492893+nigamsahab71@users.noreply.github.com>
2024-06-28 13:01:35 +0530

    Amazon Clone

---
+++ script.js
@@ -0,0 +1,16 @@
+// const product = {
+//     title: "Ball Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+// };
+// console.log(product);
+
+// alert("Welcome to clone version of Amazon");
+
+
+// alert("Please enter your name:")
+// const name = prompt("Enter your name");
+// const email = prompt("Enter your email");
+// const password = prompt("Enter your password");
+// console.log(name, email, password);
